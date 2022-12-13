83ec69e075c420553dd4c05b484f3ef87c7fdedb
Igor Cervenka <wormikk@gmail.com>
2022-12-13 10:49:55 +0100

    Updated roxygen comments and variable names

--- R/load_data.R
+++ R/load_data.R
@@ -1,3 +1,13 @@
+#' Helper for load config into list or environment
+#'
+#' @param x named list, config file parsed from json or other source
+#' @param into character string, one of c("list", "env") whether to return
+#' loaded config file as list or as an environment
+#'
+#' @return list or environment of parsed config file
+#' @export
+#'
+#' @examples
 load_into <- function(x, into) {
   match.arg(into, c("list", "env"))
   if (into == "env") {
@@ -9,31 +19,36 @@ load_into <- function(x, into) {
   }
 }
 
-#' Parse json config file
+#' Parse config file
 #'
-#' @param config charcter string, path to json config file to load
+#' @param x charcter string, list or environment to parse config from.
+#' Character string has to be a location to a json file
 #' @param into character string, one of c("list", "env") whether to return
 #' loaded config file as list or as an environment
+#' @param ... other parameters passed to load_config
 #'
 #' @return list or environment of parsed config file
 #' @export
 #'
 #' @examples
-load_json_config <- function(x, into = "list", ...) {
+load_config <- function(x, into = "list", ...) {
   UseMethod("load_json_config", x)
 }
 
-load_json_config.character <- function(x, into = "list", ...) {
+## S3 method for class 'character'
+load_config.character <- function(x, into = "list", ...) {
   stopifnot(endsWith(x, "json"))
   config_list <- rjson::fromJSON(file = x)
   return(load_into(config_list, into))
 }
 
-load_json_config.list <- function(x, into = "list", ...) {
+## S3 method for class 'list'
+load_config.list <- function(x, into = "list", ...) {
   return(load_into(x, into))
 }
 
-load_json_config.environment <- function(x, into = "list", ...) {
+## S3 method for class 'environment'
+load_config.environment <- function(x, into = "list", ...) {
   return(load_into(as.list(x), into))
 }
 
@@ -54,7 +69,7 @@ load_json_config.environment <- function(x, into = "list", ...) {
 #'
 #' @examples
 load_metadata <- function(path, params) {
-  metadata <- read_data(path) %>%
+  df <- read_data(path) %>%
     # dplyr::select(-dplyr::any_of(c("fq", "lane", "read"))) %>%
     dplyr::select(all_of(c(params$sample_colname, params$group_colname))) %>%
     base::unique() %>%
@@ -62,8 +77,8 @@ load_metadata <- function(path, params) {
       levels = params$group_levels
     )) %>%
     dplyr::arrange(!!as.symbol(params$group_colname))
-  rownames(metadata) <- NULL
-  return(metadata)
+  rownames(df) <- NULL
+  return(df)
 }
 
 #' Load normalized expression data
@@ -80,8 +95,8 @@ load_metadata <- function(path, params) {
 #'
 #' @examples
 load_data_expression <- function(path, params) {
-  expression_data <- read_data(path)
-  return(expression_data)
+  df <- read_data(path)
+  return(df)
 }
 
 #' Load data containing differetial expression
@@ -98,16 +113,16 @@ load_data_expression <- function(path, params) {
 #' @export
 #'
 #' @importFrom magrittr %>%
-#' 
+#'
 #' @examples
 load_data_diffexp <- function(path, params) {
-  diffexp_data <- purrr::map(path, function(x) {
+  df <- purrr::map(path, function(x) {
     if (nchar(x) > 0) {
       read_data(x)
     }
   }) %>%
     setNames(names(path))
-  return(diffexp_data)
+  return(df)
 }
 
 #' Load DESeq2 dds file
@@ -124,7 +139,8 @@ load_data_diffexp <- function(path, params) {
 #'
 #' @examples
 load_data_deseq <- function(path, params) {
-  dds <- readRDS(path)
+  df <- readRDS(path)
+  return(df)
 }
 
 #' Load data from cuffdiff analysis
@@ -141,33 +157,33 @@ load_data_deseq <- function(path, params) {
 #'
 #' @examples
 load_data_cuffdiff <- function(path, params) {
-  cufdiff_diff <- read_data_cuffdiff(append_dir_slash(path))
-  return(cuffdiff_diff)
+  df <- read_data_cuffdiff(append_dir_slash(path))
+  return(df)
 }
 
 #' Load data from clusterProfiler analysis
 #'
 #' Wrapper function for loading data that exposes common interface
 #' consisting of path to file/directory and parameter list parsed from json
-#' config file. Accepts files from 
+#' config file. Accepts files from
 #' https://github.com/icervenka/clusterprofiler_reports_snakemake
 #'
 #' @param path character string, path to directory with
 #' clusterprofiler_reports_snakemake csv output. Will search the path
 #' recursively, see collate_pathways_cp for more information
 #' @param params list of parameters parsed from config file. Requires
-#' cp_pathways_pattern_to be present
+#' cp_pathways_pattern to be present
 #'
 #' @return data frame
 #' @export
 #'
 #' @examples
 load_pathways_cp <- function(path, params) {
-  cp_pathways <- collate_pathways_cp(
+  pathway_data <- collate_pathways_cp(
     append_dir_slash(path),
     pattern = params$cp_pathways_pattern
   )
-  return(cp_pathways)
+  return(pathway_data)
 }
 
 #' Load data from GSEA analysis
@@ -180,7 +196,7 @@ load_pathways_cp <- function(path, params) {
 #' Will search the path recursively, see collate_pathways_gsea for more
 #' information
 #' @param params list of parameters parsed from config file. Requires
-#' pvalue_threshold to be present
+#' gsea_pathways_pattern and pvalue_threshold to be present
 #'
 #' @return data frame
 #' @export
@@ -188,38 +204,61 @@ load_pathways_cp <- function(path, params) {
 #'
 #' @examples
 load_pathways_gsea <- function(path, params) {
-  gsea_data <- collate_pathways_gsea(
+  pathway_data <- collate_pathways_gsea(
     dir = append_dir_slash(path),
+    pattern = params$gsea_pathways_pattern,
     pvalue_threshold = params$gsea_fdr_cutoff
   )
-  return(gsea_data)
+  return(pathway_data)
 }
 
-#' Title
+#' Load data from IPA analysis
 #'
-#' @param path
-#' @param params
+#' Wrapper function for loading data that exposes common interface
+#' consisting of path to file/directory and parameter list parsed from json
+#' config file. Accepts data from Qiagen IPA analysis that were processed by
+#' github.com/icervenka/ipa_reports_snakemake.
 #'
-#' @return
+#' @param path character string, path to directory with IPA analysis.
+#' Will search the path recursively
+#' @param params list of parameters parsed from config file. Requires
+#' ipa_pathways_pattern and ipa_rank_by to be present. ipa_rank_by denotes the
+#' column name to rank the pathways by
+#'
+#' @return data frame
 #' @export
 #'
 #' @examples
 load_pathways_ipa <- function(path, params) {
-  ipa_data <- collate_pathways_ipa(append_dir_slash(path), params$ipa_rank_by)
+  pathway_data <- collate_pathways_ipa(
+    append_dir_slash(pathway_data),
+    pattern = params$ipa_pathways_pattern
+    rank_by = params$ipa_rank_by
+  )
   return(ipa_data)
 }
 
-#' Title
+#' Load data from AmiGO analysis
 #'
-#' @param path
-#' @param params
+#' Wrapper function for loading data that exposes common interface
+#' consisting of path to file/directory and parameter list parsed from json
+#' config file. Accepts data from AmiGO analysis in the form of json files
+#' due to the preservation of hierarchical pathway analysis.
 #'
-#' @return
+#' @param path character string, path to directory with AmiGO analysis.
+#' Will search the path recursively
+#' @param params list of parameters parsed from config file. Requires
+#' amigo_pathways_pattern and amigo_filter_level_up_to to be present
+#'
+#' @return data frame
 #' @export
 #'
 #' @examples
 load_pathways_amigo <- function(path, params) {
-
+  pathway_data <- collate_pathways_amigo(
+    append_dir_slash(path),
+    pattern = params$amigo_pathways_pattern,
+    filter_level_up_to = params$filter_level_up_to)
 }
 
 #' Load dire analysis files
@@ -232,19 +271,19 @@ load_pathways_amigo <- function(path, params) {
 #' analysis. Will search the path recursively, see collate_pathways_dire for
 #' more information
 #' @param params list of parameters parsed from config file. Requires
-#' dire_pattern and dire_sheet_name to be present
+#' dire_pathways_pattern and dire_sheet_name to be present
 #'
 #' @return data frame
 #' @export
 #'
 #' @examples
 load_pathways_dire <- function(path, params) {
-  dire <- collate_pathways_dire(
+  pathway_data <- collate_pathways_dire(
     path,
-    params$dire_pattern,
-    params$dire_sheet_name
+    pattern = params$dire_pathways_pattern,
+    sheet_name = params$dire_sheet_name
   )
-  return(dire)
+  return(pathway_data)
 }
 
 #' Load user specified gene lists from json file
@@ -264,7 +303,7 @@ load_pathways_dire <- function(path, params) {
 #' @export
 #'
 #' @importFrom magrittr %>%
-#' 
+#'
 #' @examples
 load_gene_lists <- function(path, params) {
   if (nchar(params$modify_gene_lists) == 0) {
@@ -346,7 +385,7 @@ load_all_data <- function(input_config,
 
   paths <- load_json_config(input_config)
   params <- load_json_config(param_config)
-  
+
   data <- list()
   purrr::walk2(names(input_config), input_config, function(x, y) {
     if (nchar(y) > 0) {
@@ -354,10 +393,5 @@ load_all_data <- function(input_config,
     }
   })
 
-  if (into == "list") {
-    out <- data()
-  } else {
-    list2env(data, envir = out)
-  }
-  return(out)
+  return(load_into(data, into))
 }
