19cb9672fa02588f664f8e224b009e64dfca158d
Igor Cervenka <wormikk@gmail.com>
2023-05-18 13:54:03 +0200

    PCA function fix

--- R/pca.R
+++ R/pca.R
@@ -17,14 +17,14 @@
 plot_pca_deseq <- function(dds,
                            norm = "vst",
                            ...) {
-
+  
   # Normalized the DESeq data
   if (norm == "vst") {
     dds_norm <- DESeq2::vst(dds, blind = FALSE)
   } else if (norm == "rlog") {
     dds_norm <- DESeq2::rlog(dds, blind = FALSE)
   }
-
+  
   # Calculate PCA, return only values without plot
   pca_data <- DESeq2::plotPCA(
     dds_norm,
@@ -32,21 +32,21 @@ plot_pca_deseq <- function(dds,
     returnData = TRUE
   ) %>%
     tibble::rownames_to_column(var = "id")
-
+  
   # Create numeric values for variance explained to pass to axis labels
   percent_var_labs <- round(100 * attr(pca_data, "percentVar"))
-
+  
   # Calculate coordinates for segments connecting sample points to group
   # centers
   segments <- pca_data %>%
     dplyr::group_by(!!as.symbol(group)) %>%
     dplyr::summarise(xend = mean(PC1), yend = mean(PC2))
-
+  
   # Extract PCA data in format for generic PCA plotting function
   pca_data <- pca_data %>%
     dplyr::select(dplyr::matches(paste0("^", group, "$")), id, PC1, PC2) %>%
     dplyr::left_join(segments, by = group)
-
+  
   # Plot using generic PCA plotting function
   p <- plot_pca(
     pca_data,
@@ -73,6 +73,7 @@ plot_pca_deseq <- function(dds,
 #' @examples
 plot_pca_common <- function(expression_data,
                             metadata,
+                            group = group,
                             ...) {
   # calculate PCA components for samples specified in metadata
   pca <- prcomp(
@@ -82,21 +83,23 @@ plot_pca_common <- function(expression_data,
     ),
     scale = TRUE
   )
-
+  
   # Create final PCA data frame to plot
   pca_data <- as.data.frame(pca$x) %>%
     tibble::rownames_to_column("id") %>%
     dplyr::left_join(metadata, by = c("id" = "sample"))
-
+  
+  
   # Calculate coordinates for segments connecting sample points to group
   # centers
   segments <- pca_data %>%
-    dplyr::group_by(!!as.symbol(group)) %>%
+    dplyr::group_by({{ group }}) %>%
     dplyr::summarise(xend = mean(PC1), yend = mean(PC2))
+  
 
   # Join with the PCA data plot
   pca_data <- pca_data %>%
-    dplyr::left_join(segments, by = group)
+    dplyr::left_join(segments, by = join_by({{ group }} == {{ group }}))
 
   # Create numeric values for variance explained to pass to axis labels
   percent_var_labs <- round(pca$sdev / sum(pca$sdev) * 100, 2)
@@ -104,18 +107,29 @@ plot_pca_common <- function(expression_data,
     colnames(pca),
     "(",
     paste(as.character(percent_var_labs),
-      "%",
-      ")",
-      sep = ""
+          "%",
+          ")",
+          sep = ""
     )
   )
-
+  
   # Plot using generic PCA plotting function
-  p <- plot_pca(
-    pca_data,
-    percent_var_labs,
-    !!!list(...)
-  )
+  # Special case when no dots are supplied
+  if (length(list(...)) == 0) {
+    p <- plot_pca(
+      pca_data,
+      percent_var_labs,
+      group = group
+    )
+  } else {
+    p <- plot_pca(
+      pca_data,
+      percent_var_labs,
+      group = group,
+      !!!list(...)
+    )
+  }
+  
   return(p)
 }
 
@@ -125,10 +139,10 @@ plot_pca_common <- function(expression_data,
 #' @param percent_var_labs double vector of length 2, numeric values of
 #' explained variance for the PCA plot axes
 #' @param group
-#' @param plot_center logical, whether to plot the center of group in the PCA
+#' @param group_center logical, whether to plot the center of group in the PCA
 #' plot
 #' @param linetype character string, a valid linetype for ggplot2. If the
-#' plot_center is set to TRUE, this will determine the line type of drawn
+#' group_center is set to TRUE, this will determine the line type of drawn
 #' connections between sample points to group center. default: solid
 #' be
 #' @param palette character vector of the same length as the number of groups in
@@ -143,27 +157,29 @@ plot_pca_common <- function(expression_data,
 #' @examples
 plot_pca <- function(pca_data,
                      percent_var_labs,
-                     group = "group",
-                     plot_center = TRUE,
+                     group = group,
+                     ahoj = TRUE,
+                     group_center = TRUE,
                      linetype = "solid",
                      palette = NULL) {
 
   # get number of colors needed to display
-  no_colors <- pca_data[, group] %>%
+  no_colors <- pca_data %>%
+    dplyr::pull({{ group }}) %>%
     unique() %>%
     length()
-
+  
   # Create basic PCA plot
   p <- pca_data %>%
     ggplot2::ggplot(ggplot2::aes(
       PC1,
       PC2,
-      color = !!as.symbol(group)
+      color = {{ group }}
     )) +
     ggplot2::geom_point(size = 3)
-
+  
   # Add group center and segments if specified by the user
-  if (plot_center == TRUE) {
+  if (group_center == TRUE) {
     p <- p +
       ggplot2::geom_segment(ggplot2::aes(
         x = PC1,
@@ -179,14 +195,14 @@ plot_pca <- function(pca_data,
         size = 1
       )
   }
-
+  
   # Add axes labels and style the graph
   p <- p +
     ggplot2::xlab(paste0("PC1: ", percent_var_labs[1], "% variance")) +
     ggplot2::ylab(paste0("PC2: ", percent_var_labs[2], "% variance")) +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "top")
-
+  
   # Apply custom palette
   if (is.null(palette)) {
     p <- p +
