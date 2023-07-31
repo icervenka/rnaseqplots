ce1113c15efa92a685680331552eddc86b96abbd
Igor Cervenka <wormikk@gmail.com>
2023-07-31 12:12:54 +0200

    Fix pca percent variance calculation error

--- R/pca.R
+++ R/pca.R
@@ -17,14 +17,14 @@
 plot_pca_deseq <- function(dds,
                            norm = "vst",
                            ...) {
-  
+
   # Normalized the DESeq data
   if (norm == "vst") {
     dds_norm <- DESeq2::vst(dds, blind = FALSE)
   } else if (norm == "rlog") {
     dds_norm <- DESeq2::rlog(dds, blind = FALSE)
   }
-  
+
   # Calculate PCA, return only values without plot
   pca_data <- DESeq2::plotPCA(
     dds_norm,
@@ -32,21 +32,21 @@ plot_pca_deseq <- function(dds,
     returnData = TRUE
   ) %>%
     tibble::rownames_to_column(var = "id")
-  
+
   # Create numeric values for variance explained to pass to axis labels
   percent_var_labs <- round(100 * attr(pca_data, "percentVar"))
-  
+
   # Calculate coordinates for segments connecting sample points to group
   # centers
   segments <- pca_data %>%
     dplyr::group_by(!!as.symbol(group)) %>%
     dplyr::summarise(xend = mean(PC1), yend = mean(PC2))
-  
+
   # Extract PCA data in format for generic PCA plotting function
   pca_data <- pca_data %>%
     dplyr::select(dplyr::matches(paste0("^", group, "$")), id, PC1, PC2) %>%
     dplyr::left_join(segments, by = group)
-  
+
   # Plot using generic PCA plotting function
   p <- plot_pca(
     pca_data,
@@ -83,26 +83,26 @@ plot_pca_common <- function(expression_data,
     ),
     scale = TRUE
   )
-  
+
   # Create final PCA data frame to plot
   pca_data <- as.data.frame(pca$x) %>%
     tibble::rownames_to_column("id") %>%
     dplyr::left_join(metadata, by = c("id" = "sample"))
-  
-  
+
+
   # Calculate coordinates for segments connecting sample points to group
   # centers
   segments <- pca_data %>%
     dplyr::group_by({{ group }}) %>%
     dplyr::summarise(xend = mean(PC1), yend = mean(PC2))
-  
+
 
   # Join with the PCA data plot
   pca_data <- pca_data %>%
     dplyr::left_join(segments, by = join_by({{ group }} == {{ group }}))
 
   # Create numeric values for variance explained to pass to axis labels
-  percent_var_labs <- round(pca$sdev / sum(pca$sdev) * 100, 2)
+  percent_var_labs <- round(pca$sdev^2 / sum(pca$sdev^2) * 100, 2)
   percent_var_labs <- paste0(
     colnames(pca),
     "(",
@@ -112,7 +112,7 @@ plot_pca_common <- function(expression_data,
           sep = ""
     )
   )
-  
+
   # Plot using generic PCA plotting function
   # Special case when no dots are supplied
   if (length(list(...)) == 0) {
@@ -129,7 +129,7 @@ plot_pca_common <- function(expression_data,
       ...
     )
   }
-  
+
   return(p)
 }
 
@@ -168,7 +168,7 @@ plot_pca <- function(pca_data,
     dplyr::pull({{ group }}) %>%
     unique() %>%
     length()
-  
+
   # Create basic PCA plot
   p <- pca_data %>%
     ggplot2::ggplot(ggplot2::aes(
@@ -177,7 +177,7 @@ plot_pca <- function(pca_data,
       color = {{ group }}
     )) +
     ggplot2::geom_point(size = 3)
-  
+
   # Add group center and segments if specified by the user
   if (group_center == TRUE) {
     p <- p +
@@ -195,14 +195,14 @@ plot_pca <- function(pca_data,
         size = geom_point_size
       )
   }
-  
+
   # Add axes labels and style the graph
   p <- p +
     ggplot2::xlab(paste0("PC1: ", percent_var_labs[1], " variance")) +
     ggplot2::ylab(paste0("PC2: ", percent_var_labs[2], " variance")) +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "top")
-  
+
   # Apply custom palette
   if (is.null(palette)) {
     p <- p +
